7005841501d8c4c6db7459e9cf66d846e7e5c002
Rob Pongsajapan <rap42@georgetown.edu>
2014-02-20 17:24:38 -0500

    blog archive mods

--- archive.php
+++ archive.php
@@ -9,13 +9,38 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
+<div id="page-header" class="col-md-12">
+    <div class="outer">
+        <div class="scrim">
+        </div>
+
+        <div class="photo">
+            <div class="carousel-inner">
+                <div class="page-title container">
+                    <h1 class="entry-title">News</h1>
+                </div>
+            </div>
+        </div><!-- /.photo -->
+    </div><!-- /.outer -->
+</div>
+
+<div class="container">
+
+    <div class="row">
+    
+        <div class="col-md-4">
+<?php get_sidebar(); ?>
+        </div>
+        
+        <div class="col-md-8">
+
+	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title">
+				<h2 class="page-title">
 					<?php
 						if ( is_category() ) :
 							single_cat_title();
@@ -67,7 +92,7 @@ get_header(); ?>
 
 						endif;
 					?>
-				</h1>
+				</h2>
 				<?php
 					// Show an optional term description.
 					$term_description = term_description();
@@ -99,7 +124,12 @@ get_header(); ?>
 		<?php endif; ?>
 
 		</main><!-- #main -->
-	</section><!-- #primary -->
+	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+        </div><!-- .col-md-8 -->
+    
+    </div><!-- .row -->
+
+</div><!-- .container -->
+
 <?php get_footer(); ?>
