5138e2641c682d536c79fde2138b30f275931a39
Mohammed El-Khatib <mmelkhatib@gmail.com>
2017-06-28 14:28:09 -0400

    corrected path for partials location on single.php and page.php

--- single.php
+++ single.php
@@ -25,12 +25,12 @@ get_header(); ?>
 <div class="container">
 
     <div class="row">
-        
+
         <div class="col-md-4">
 <?php get_sidebar(); ?>
         </div>
-       
-        
+
+
         <div class="col-md-8">
 
 	<div id="primary" class="content-area">
@@ -38,7 +38,7 @@ get_header(); ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'content', 'single' ); ?>
+			<?php get_template_part( 'partials/content', 'single' ); ?>
 
 			<?php //futures_post_nav(); ?>
 
@@ -55,7 +55,7 @@ get_header(); ?>
 	</div><!-- #primary -->
 
         </div><!-- .col-md-8 -->
-    
+
     </div><!-- .row -->
 
 </div><!-- .container -->
