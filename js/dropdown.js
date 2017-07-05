67f11b5d48ef1818794903acc1c0a8d4b78bfe93
Mohammed El-Khatib <mmelkhatib@gmail.com>
2017-07-05 16:49:35 -0400

    refractoring and tweaks to the desktop and mobile versions of the nav.

--- js/dropdown.js
+++ js/dropdown.js
@@ -4,6 +4,7 @@ var subLink = $('.sub-menu').children('li');
 var navWidth = $('.navbar-nav').width();
 
 function desktopDropDown() {
+  //clone all top menu links and prepend them to the sub menu links
   $(".navbar-nav li:first-child").each(function(){
       var parent = $(this).parent('ul').parent('.dropdown');
       var copy = parent.clone();
@@ -11,10 +12,25 @@ function desktopDropDown() {
       $(copy).children().removeClass('dropdown-toggle').removeAttr( "data-toggle" );
       $(this).before(copy);
   });
+  // add a chevron if  menu links to a submenu
+    $('.dropdown-submenu').children('a').append('<span class="glyphicon glyphicon-chevron-right"></span>');
+    // ADD SLIDEDOWN ANIMATION TO DROPDOWN //
+   $('.dropdown').on("show.bs.dropdown", function (e) {
+     console.log("I am showing!");
+       $(this).find('.dropdown-menu').slideDown();
+   });
 
-  // $('.menu-item-has-children > a').attr('href' ,'#');
-
+   // ADD SLIDEUP ANIMATION TO DROPDOWN //
+   $('.dropdown').on('hide.bs.dropdown', function (e) {
+       $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+   });
 }
+
 $(document).ready(function(){
+  //instantiate function on doc ready//
   desktopDropDown();
+
+
+
+
 });
