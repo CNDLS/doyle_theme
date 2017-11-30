20fe2df6d2d7cc22b77ea8f47ed66e0e52958b7a
Brian Blosser <bpb54321@gmail.com>
2017-11-30 12:12:27 -0500

    Redid Bootstrap layout so it was more correct, added a min-height to .loop-item because the reason the grid was breaking was because the columns were different heights because of different image aspect ratios.

--- page-templates/content-side-image.php
+++ page-templates/content-side-image.php
@@ -14,16 +14,19 @@ get_header(); ?>
 	<div id="main" class="container">
 		<div class="row">
 			<div class="col-md-12 box">
-				<div class="row">
-					<h1 class="entry-title"><?php the_title(); ?></h1>
-					<div class="col-md-4">
-						<?php the_post_thumbnail( 'post-thumbnail', array( 'class' => 'img-responsive responsive--full' ) ); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<div class="row">
+						<h1 class="entry-title"><?php the_title(); ?></h1>
+						<div class="col-md-4">
+							<?php the_post_thumbnail( 'post-thumbnail', array( 'class' => 'img-responsive responsive--full' ) ); ?>
+						</div>
+						<div class="col-md-8">
+							<?php the_content(); ?>
+						</div>
 					</div>
-					<div class="col-md-8">
-						<?php while ( have_posts() ) : the_post(); ?>
-							<?php get_template_part( 'partials/content', 'page' ); ?>
-						<?php endwhile; // end of the loop.?>
-					</div>
+				<?php endwhile; // end of the loop.?>
+				<div class="row loop-container">
+					<?php get_template_part( 'partials/content', 'page' ); ?>
 				</div>
 			</div>
 		</div>
