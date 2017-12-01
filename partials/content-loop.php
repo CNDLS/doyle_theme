36199c49c1440895e5b87eb782a723b2d197ee65
Brian Blosser <bpb54321@gmail.com>
2017-12-01 11:24:07 -0500

    Printing out the thumbnail image myself.

--- partials/content-loop.php
+++ partials/content-loop.php
@@ -7,7 +7,13 @@
 
 			<?php if ( has_post_thumbnail() ): ?>
 				<div class="col-md-5">
-					<?php the_post_thumbnail( 'post-thumbnail', array( 'class' => 'img-responsive responsive--full' ) ); ?>
+					<?php
+					$thumbnail_id    = get_post_thumbnail_id();
+					$image_src_array = wp_get_attachment_image_src( $thumbnail_id, 'thumbnail' );
+					$image_src       = $image_src_array[0];
+					?>
+					<img src="<?php echo $image_src; ?>" alt="<?php the_title(); ?>"
+						 class="img-responsive responsive--full">
 				</div>
 				<div class="col-md-7 loop-body">
 					<?php the_excerpt(); ?>
